4c7dc2819a0aba9697ebc1f13290ddf94445f690
Atanas Yonkov <atanasyonkov1@gmail.com>
2025-06-16 16:30:42 +0300

    feat: add Posts component to display latest posts on the homepage

--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/hero'
+import Posts from '@/components/Posts'
 
 export default function Home() {
     return (
         <main>
             <Hero />
+            <Posts posts={[]} />
         </main>
     )
 }
