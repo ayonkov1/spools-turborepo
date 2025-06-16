3ac7cf026f237e0d101db1f98d618cf16293c7bb
Atanas Yonkov <atanasyonkov1@gmail.com>
2025-06-16 20:32:43 +0300

    feat: enhance PostCard component with title display and link to post details, add default thumbnail image

--- apps/front/src/components/postCard.tsx
+++ apps/front/src/components/postCard.tsx
@@ -1,34 +1,36 @@
 import { Post } from '@/lib/types/modelTypes'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = Partial<Post>
 
 const PostCard = ({ id, slug, thumbnail, title, content, author, createdAt }: Props) => {
     return (
-        <div className="bg-white/30 backdrop-blur-md p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow border border-white/20 ">
-            {thumbnail && (
-                <Image
-                    src={thumbnail}
-                    alt={title || 'Post thumbnail'}
-                    width={600}
-                    height={192}
-                    className="w-full h-48 object-cover rounded-md mb-4"
-                />
-            )}
+        <div className="backdrop-blur-md p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow border border-white/20 flex flex-col mx h-full">
+            <Image
+                src={thumbnail || '/no-image.png'}
+                alt={title || 'Post thumbnail'}
+                width={600}
+                height={192}
+                className="w-full h-48 object-cover rounded-md mb-4"
+            />
             <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
-            <p className="text-gray-800 mt-2">{content?.slice(0, 100)}...</p>
-            <div className="mt-4 text-sm text-gray-800">
-                <p>By {author?.name || 'John Doe'}</p>
-                <p>{createdAt ? new Date(createdAt).toLocaleDateString('de-DE') : ''}</p>
+            <div className="text-gray-600 text-sm mt-1 mb-2 flex-grow flex flex-col">
+                <p className="text-gray-800 mt-2">{content?.slice(0, 100)}...</p>
+                <div className="mt-4 text-sm text-gray-800">
+                    <p>By {author?.name || 'John Doe'}</p>
+                    <p>{createdAt ? new Date(createdAt).toLocaleDateString('de-DE') : ''}</p>
+                </div>
+                <div className="flex-grow" />
                 {slug && (
-                    <p>
-                        <span className="font-medium">Slug:</span> {slug}
-                    </p>
-                )}
-                {id && (
-                    <p>
-                        <span className="font-medium">ID:</span> {id}
-                    </p>
+                    <div className="flex justify-end mt-4">
+                        <Link
+                            href={`/posts/${slug}/${id}`}
+                            className="text-blue-500 hover:underline"
+                        >
+                            Read more
+                        </Link>
+                    </div>
                 )}
             </div>
         </div>
--- apps/front/src/lib/fetchGraphQL.ts
+++ apps/front/src/lib/fetchGraphQL.ts
@@ -6,7 +6,7 @@ export const fetchGraphQL = async (query: string, variables = {}) => {
         headers: {
             'Content-Type': 'application/json',
             'authorization':
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJpYXQiOjE3NTAwODExMTcsImV4cCI6MTc1MDA4NDcxN30.WUzMeVgdyol8HY7c8QyeQti_z6vvnR9_TMdolx9ggvM',
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJpYXQiOjE3NTAwOTQxMjIsImV4cCI6MTc1MDA5NzcyMn0.yclM91cW6oUXst1KxYSB6chlDpHtYwrWTRmP9tLOrGU',
         },
         body: JSON.stringify({
             query,
